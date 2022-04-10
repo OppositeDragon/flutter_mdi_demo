8f3d74632f48a12b590c91d4069c63ae1049bae7
OppositeDragon <stee-ven_94@outlook.com>
2022-04-10 16:42:05 -0500

    branch cleaned

--- README.md
+++
@@ -1,5 +0,0 @@
-# flutter_mdi_demo
-
-A new Flutter project.
-
-Attempting to create a MultiDocument Interface using flutter.
--- lib/models/dock_icons.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_mdi_demo/models/desktop_icons.dart';
-import 'package:flutter_mdi_demo/widgets/desktop.dart';
-
-class DockIcon{
-	late DesktopIcon icon;
-	late int numInstancias;
-	DockIcon(this.icon,this.numInstancias);
-}
